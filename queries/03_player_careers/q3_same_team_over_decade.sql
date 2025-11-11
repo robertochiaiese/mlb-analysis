26b98112c6b8281e20c091c7df3a461ac058adbf
Roberto <chiaieseroberto@gmail.com>
2025-11-11 19:36:07 +0100

    Update q3_same_team_over_decade.sql

--- queries/03_player_careers/q3_same_team_over_decade.sql
+++ queries/03_player_careers/q3_same_team_over_decade.sql
@@ -3,6 +3,7 @@
 ---------------also played for over a decade? --------------
 ---------------------------------------------------------- */
 
+USE MLBdb;
 
 WITH players_years AS (SELECT p.nameGiven, p.playerID, MIN(yearID) AS first_year, MAX(yearID)  AS last_year
 					   FROM players p 
@@ -13,3 +14,7 @@ WITH players_years AS (SELECT p.nameGiven, p.playerID, MIN(yearID) AS first_year
 					   FROM players_years py
 							LEFT JOIN  salaries s2 ON py.playerID = s2.playerID AND py.first_year = s2.yearID
 							LEFT JOIN  salaries s3 ON py.playerID = s3.playerID AND py.last_year = s3.yearID)
+
+SELECT *
+FROM track_players
+WHERE first_team = last_team AND last_year - first_year >= 10;
