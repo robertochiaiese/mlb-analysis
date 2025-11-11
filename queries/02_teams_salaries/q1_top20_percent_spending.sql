06c9d9898c292c7ee4cc4c97e9bf330f854b902e
Roberto <chiaieseroberto@gmail.com>
2025-11-11 19:11:23 +0100

    Create q1_top20_percent_spending.sql

---
+++ queries/02_teams_salaries/q1_top20_percent_spending.sql
@@ -0,0 +1,23 @@
+/* -----------------------------------------------------------------------
+----------TTop 20% of teams in terms of average annual spending-----------
+------------------------------------------------------------------------- */
+
+USE MBLdb;
+
+WITH total_salaries AS (SELECT  yearID, teamID, SUM(salary) AS total_salary
+						FROM 	salaries
+						GROUP BY yearID, teamID),
+                        
+	 avg_salaries 	AS (SELECT	
+							teamID,
+							ROUND (AVG( total_salary )/1000000, 1) AS average_salary_million
+						FROM total_salaries
+						GROUP BY teamID)
+
+SELECT	*	
+FROM ( SELECT teamID,
+			  average_salary_million,
+              NTILE(5) OVER(ORDER BY average_salary_million DESC) AS classification
+		FROM avg_salaries
+        ) t
+WHERE classification = 1;
